53f7017e6c6f917185fa902df45a1c8d5ae73469
DONG IL <cappu159@gmail.com>
2023-01-15 20:53:15 +0900

    ✨ feat(*): initiallized

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParans: "avoid",
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: true,
+  trailingComma: "none",
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+};
+
+module.exports = options;
